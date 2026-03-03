7aaffc7b9c6b20c1573eb333dd82247489934bd6
sgvmitev <viktor4o256@gmal.com>
2026-03-03 08:57:51 -0800

    Initial Commit

---
+++ pixel partyUITests/pixel_partyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  pixel_partyUITestsLaunchTests.swift
+//  pixel partyUITests
+//
+//  Created by Mitev, Viktor on 03/03/2026.
+//
+
+import XCTest
+
+final class pixel_partyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
