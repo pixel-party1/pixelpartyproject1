6db1e023ed7fc08bcccf883ef2fdd41dc7b29bb4
tat.png <leahyt.maya@outlook.com>
2026-03-16 15:35:28 +0000

    testing the push and pull functionality

--- pixel party/MainMenuViewController.swift
+++ pixel party/MainMenuViewController.swift
@@ -21,5 +21,6 @@ class MainMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // test 
     }
 }
