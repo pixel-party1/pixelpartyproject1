f28970eefaa29707aeb86f2436d6e31de2d48703
Anna-Maria Agard <annamaria.agard@gmail.com>
2026-03-18 14:55:06 +0000

    started adding fonts to the game however still needs some work

---
+++ pixel party/MemoryMatchMainMenuViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryMatchMainMenuViewController.swift
+//  pixel party
+//
+//  Created by Agard, Anna-Maria on 18/03/2026.
+//
+
+import UIKit
+
+class MemoryMatchMainMenuViewController: UIViewController {
+    
+    
+    @IBOutlet weak var memoryMatchTitleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        memoryMatchTitleLabel.font = UIFont(name: "KenneyBlocks", size: 28)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
