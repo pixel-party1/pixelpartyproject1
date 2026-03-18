69916e0459a26e7c8e589d60b9205ca1c82faa95
Ellis Roome <roome1331@gmail.com>
2026-03-18 14:00:20 +0000

    Readded Structs and Assets

---
+++ pixel party/Structs/GameScore.swift
@@ -0,0 +1,13 @@
+//
+//  GameScore.swift
+//  pixel party
+//
+//  Created by Mitev, Viktor on 16/03/2026.
+//
+
+import Foundation
+
+struct GameScore: Codable {
+    let playerName: String
+    let score: Int
+}
