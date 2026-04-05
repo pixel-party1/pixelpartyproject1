8ade562d5278d4971898fdda967cdfb50cccbb99
tat.png <leahyt.maya@outlook.com>
2026-04-05 17:31:09 +0100

    added main menu options and VC for the mode

---
+++ pixel party/MazeGameEasyModeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MazeGameEasyModeViewController.swift
+//  pixel party
+//
+//  Created by Tatyana Leahy on 05/04/2026.
+//
+
+import UIKit
+
+class MazeGameEasyModeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ pixel party/MazeGameHardModeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MazeGameHardModeViewController.swift
+//  pixel party
+//
+//  Created by Tatyana Leahy on 05/04/2026.
+//
+
+import UIKit
+
+class MazeGameHardModeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
