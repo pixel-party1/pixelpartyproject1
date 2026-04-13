1d355cb32441130171d8af7fe586daa786f5610a
Anna-Maria Agard <annamaria.agard@gmail.com>
2026-04-13 14:52:27 +0100

    added in timers for each of the levels

--- pixel party/MemoryMatchMediumModeViewController.swift
+++ pixel party/MemoryMatchMediumModeViewController.swift
@@ -14,10 +14,29 @@ class MemoryMatchMediumModeViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var clockDisplay: UILabel!
+    
+    var timer: Timer?
+    var timerCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        clockDisplay.text = "00"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerFired), userInfo: nil, repeats: true)
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func timerFired() {
+        timerCount += 1
+        
+        let seconds = timerCount % 60
+        let minutes = timerCount / 60
+        
+        clockDisplay.text = String(format: "%02d:%02d", minutes,seconds)
+        clockDisplay.font = UIFont(name: "Kenney-Rocket", size: 30)
     }
     
 
