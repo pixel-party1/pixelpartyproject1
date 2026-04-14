1ba6595443af73f0677d2e12c8f5db3f8b2d987a
tat-lhy <leahyt.maya@outlook.com>
2026-04-14 15:21:58 +0100

    Revert "Added difficulty selection buttons wired to easy, medium and hard mode screens via segues"
    This reverts commit 64686f0a4474a45b5509dcb02e066aa8dd4f19bc.

--- pixel party/MemoryMatchPlayViewController.swift
+++ pixel party/MemoryMatchPlayViewController.swift
@@ -2,35 +2,36 @@
 //  MemoryMatchPlayViewController.swift
 //  pixel party
 //
+//  Created by Agard, Anna-Maria on 19/03/2026.
+//
 
 import UIKit
 
 class MemoryMatchPlayViewController: UIViewController {
-
-    @IBOutlet weak var memoryMatchSelectDifficultyLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        memoryMatchSelectDifficultyLabel.font = UIFont(name: "Kenney-Rocket", size: 28)
-    }
-
-    // MARK: - Difficulty Buttons
-
-    @IBAction func easyButtonPressed(_ sender: Any) {
-        performSegue(withIdentifier: "toEasyMode", sender: self)
-    }
-
-    @IBAction func mediumButtonPressed(_ sender: Any) {
-        performSegue(withIdentifier: "toMediumMode", sender: self)
-    }
-
-    @IBAction func hardButtonPressed(_ sender: Any) {
-        performSegue(withIdentifier: "toHardMode", sender: self)
-    }
-
-    // MARK: - Back Button
+    
+    
     @IBAction func memoryMatchPlayBackButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    
+    @IBOutlet weak var memoryMatchSelectDifficultyLabel: UILabel!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+
+        memoryMatchSelectDifficultyLabel.font = UIFont(name: "Kenney-Rocket", size: 28)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
