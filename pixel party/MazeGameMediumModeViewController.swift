8234ce3d6a0464e2c46adc0172a3dcc8fe33841d
Matthew Green <sgmgree8@liverpool.ac.uk>
2026-04-14 10:14:28 +0100

    Added timers for medium and hard modes

--- pixel party/MazeGameMediumModeViewController.swift
+++ pixel party/MazeGameMediumModeViewController.swift
@@ -14,13 +14,31 @@ class MazeGameMediumModeViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var mazeGameMediumModeTimer: UILabel!
+    
+    var timer: Timer?
+    var timerCount = 0
+    
+    
     @IBOutlet weak var mediumMazeBoardView: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerFired), userInfo: nil, repeats: true)
 
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func timerFired() {
+        timerCount += 1
+        
+        let seconds = timerCount % 60
+        let minutes = timerCount / 60
+        
+        mazeGameMediumModeTimer.text = String(format: "%02d:%02d", minutes, seconds)
+
     }
     
 
